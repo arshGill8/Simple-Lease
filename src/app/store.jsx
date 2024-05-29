385494d725675d609b894c8f8e3666c6ad10d7e8
arshgill8 <arsh95g@gmail.com>
2024-05-29 19:45:33 -0400

    fixer

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -4,11 +4,7 @@ import { tenantNameReducer } from "../features/name/tenantName";
 import { contactInfoReducer } from "../features/contactInfo";
 import { depositInfoReducer } from "../features/depositInfo";
 import { leaseTermInfoReducer } from "../features/leaseTermInfo";
-<<<<<<< HEAD
 import { rentalUnitReducer } from "../features/rentalUnit";
-=======
-import { propertyInfoReducer } from "../features/propertyInfo";
->>>>>>> 932f9ad (hi)
 import { utilityInfoReducer } from "../features/utilityInfo";
 import { tenantSignatureReducer } from "../features/signature/tenantSignature";
 import { landlordSignatureReducer } from "../features/signature/landlordSignature";
